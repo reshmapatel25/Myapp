68cb889fb6f7093be73b0c96b0f91000c7d4784a
reshmapatel25 <reshma2505@gmail.com>
2020-04-29 12:20:43 +0100

    RecipeDetailComponent added for delegation of Recipe view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component'; // <-- NgModel lives here
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
